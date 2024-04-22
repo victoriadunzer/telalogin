1f9f869dc65f1e2835539c55c2ed94ffa2196168
victoriadunzer <160753388+victoriadunzer@users.noreply.github.com>
2024-04-22 12:30:22 -0300

    Add files via upload

--- Att_Login/scripts/script.js
+++ Att_Login/scripts/script.js
@@ -1,5 +1,19 @@
+var usuario = {
+    nome: "Victoria",
+    senha: "123"
+}
 function entrar(){
     
-    var nome= document.getElementById('nome').value
-    var senha= document.getElementById('nome').value
+    var nome= document.getElementById('nome').value;
+    var senha= document.getElementById('senha').value;
+
+    var login = {
+        nome: nome,
+        senha: senha
+    }
+    if (login.nome == usuario.nome && login.senha == usuario.senha) {
+        location.replace("./prox.html");
+    } else {
+        alert ("Usuário e/ou senha incorretos");
+    }
 }
